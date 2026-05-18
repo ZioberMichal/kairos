651549300f92294851fcf8f3feca9d8ef1c65a86
ZioberMichal <ziobermichal@gmail.com>
2026-05-19 00:10:14 +0200

    🔧 Add environment-specific application properties (dev, stage, prod), refactor default properties, and enhance Flyway & JPA configurations

--- README.md
+++ README.md
@@ -12,9 +12,19 @@
 - Docker
 - Maven
 
+# Local environment
+- Run MySQL server:
+``````
+docker run --name KairosMySQL -e MYSQL_ROOT_PASSWORD=secret-password -e MYSQL_DATABASE=kairos -p 3306:3306 -d mysql:8.4
+``````
+- Connect to MySQL server
+```
+docker exec -it KairosMySQL mysql -u root -p
+```
+
 # Build and run on the Spring Boot
 ```
 mvn clean package
 Run Application from IDE or  
-mvn spring-boot:run  
+mvn spring-boot:run -Dspring-boot.run.profiles=dev 
 ```
--- src/main/resources/db/migration/mockdata/V3.01__initial_mockdata.sql
+++ src/main/resources/db/migration/mockdata/V3.01__initial_mockdata.sql
@@ -1,10 +1,10 @@
 INSERT INTO department VALUES (1, 'Sales', 'admin', NOW(), 'admin', NOW());
 INSERT INTO department VALUES (2, 'Research & Development (R&D)', 'admin', NOW(), 'admin', NOW());
 
-INSERT INTO asset VALUES (1, 'Toyota Corolla Hybrid', 'KAI-2025-0003', 1, 'admin', NOW(), 'admin', NOW());
-INSERT INTO asset VALUES (2, 'Volkswagen ID.7', 'KAI-VEH-2025-0003', 2, 'admin', NOW(), 'admin', NOW());
-INSERT INTO asset VALUES (3, 'Cisco Meraki MR36 Wi-Fi Access Point', 'KAI-2025-0012', 3, 'admin', NOW(), 'admin', NOW());
-INSERT INTO asset VALUES (4, 'Company Building - Warsaw HQ', 'KAI-2025-0016', 13, 'admin', NOW(), 'admin', NOW());
+INSERT INTO asset VALUES (1, 'Toyota Corolla Hybrid', 'KAI-2025-0003', 'admin', NOW(), 'admin', NOW());
+INSERT INTO asset VALUES (2, 'Volkswagen ID.7', 'KAI-VEH-2025-0003', 'admin', NOW(), 'admin', NOW());
+INSERT INTO asset VALUES (3, 'Cisco Meraki MR36 Wi-Fi Access Point', 'KAI-2025-0012', 'admin', NOW(), 'admin', NOW());
+INSERT INTO asset VALUES (4, 'Company Building - Warsaw HQ', 'KAI-2025-0016', 'admin', NOW(), 'admin', NOW());
 
 INSERT INTO employee VALUES (1, 'Adam', 'Mickiewicz', 'amic', 'admin', NOW(), 'admin', NOW());
 INSERT INTO employee VALUES (2, 'Bolesław', 'Prus', 'bpru', 'admin', NOW(), 'admin', NOW());
